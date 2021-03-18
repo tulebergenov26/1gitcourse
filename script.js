0bde772a229c70d4cac7363b912a92461ae437f1
Ali <190103305@stu.sdu.edu.kz>
2021-03-18 22:13:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+/* "use strict";
+
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 8);
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: "25",
+    isMarried: false
+};
+console.log(obj.age);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[4]); */
+
+/* "use strict"; */
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here ?");
+console.log(result); */
+
+/* const answer = +prompt("Вам есть 18 ?", "");
+console.log(answer + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = prompt("Сколько вам лет", "");
+
+document.write(answers); */
+
+
+
+
+/* Интерполяция ES6 */
+
+"use strict";
+
+const category = "toys";
+
+console.log(`https://someurl/${category}/5`);
+
+const user = "Aseke";
+
+alert(`Hello, ${user}`);
+
+
+
